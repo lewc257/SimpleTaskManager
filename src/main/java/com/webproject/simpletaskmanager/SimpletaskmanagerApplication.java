0a2d38973faad985ff70282ca1ed4624b4410999
Lewis <djl700@live.co.uk>
2019-10-26 00:09:21 +0100

    First Project Commit

---
+++ src/main/java/com/webproject/simpletaskmanager/SimpletaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.simpletaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpletaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpletaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webproject/simpletaskmanager/SimpletaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.simpletaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
