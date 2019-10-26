c4605742da99c6178a83d375a1fa6cafb3ca24ac
Lewis <djl700@live.co.uk>
2019-10-26 01:08:20 +0100

    created the database schema

---
+++ schema.sql
@@ -0,0 +1,33 @@
+drop table if exists user_account;
+drop table if exists customer;
+drop table if exists task;
+
+create table user_account(
+	user_id int auto_increment,
+	username varchar(255) not null unique,
+	password varchar(255) not null,
+	created timestamp not null default current_timestamp,
+	primary key(user_id)
+);
+
+create table customer(
+	customer_id int auto_increment,
+	user_id int not null unique,
+	first_name varchar(255) not null,
+	last_name varchar(255) not null,
+	personal_email varchar(255) not null unique,
+	created timestamp not null default current_timestamp,
+	primary key(customer_id),
+	foreign key (user_id) references user_account(user_id)
+);
+
+
+create table task(
+	task_id int auto_increment,
+	user_id int not null,
+	name varchar(255) not null,
+	status boolean default 0,
+	created timestamp not null default current_timestamp,
+	primary key (task_id),
+	foreign key (user_id) references user_account(user_id)
+);
--- src/main/java/com/webproject/simpletaskmanager/SimpletaskmanagerApplication.java
+++ src/main/java/com/webproject/simpletaskmanager/SimpletaskmanagerApplication.java
@@ -9,5 +9,4 @@ public class SimpletaskmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpletaskmanagerApplication.class, args);
 	}
-
 }
