6ca2b9e332b61909bcf5c9b65176fb13d19af9ac
Lewis <djl700@live.co.uk>
2019-10-30 16:10:04 +0000

    building page links

--- src/main/java/com/webproject/simpletaskmanager/controllers/MainController.java
+++ src/main/java/com/webproject/simpletaskmanager/controllers/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.webproject.simpletaskmanager.entities.Useraccount;
 import com.webproject.simpletaskmanager.repositoriesdao.TaskDAO;
 import com.webproject.simpletaskmanager.repositoriesdao.UseraccountDAO;
 
@@ -24,7 +25,8 @@ public class MainController {
 	
 	@RequestMapping(value="/dashboard", method=RequestMethod.GET)
 	public String dashboardPage(HttpServletRequest request, Model model) {
-		
+		Useraccount user = (Useraccount) model.asMap().get("user");
+		System.out.println("Logged in user: " + user);
 		return "dashboard";
 	}
 }
